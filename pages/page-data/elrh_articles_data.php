456f0d0c9569d04d391b59e13d76e9675ef61bbb
Alois Sečkár <ellrohir@seznam.cz>
2016-01-17 21:52:00 +0100

    Bugfix
    Forgotten change in article detail SQL query

--- pages/page-data/elrh_articles_data.php
+++ pages/page-data/elrh_articles_data.php
@@ -14,7 +14,7 @@ class ELRHPageData {
 				// notify content renderer, there will be only one article
 				$data["single"] = true;
 				// try to find particular article
-				$data["entry"] = ELRHDataExtractor::retrieveRow($mysqli, "SELECT a.id AS aid, a.author, a.cat, a.posted, a.name AS article_name, a.dscr, a.content, g.id AS gid, g.name AS gallery_name, g.images, u.u_displayed_name AS author_name FROM elrh_articles a LEFT JOIN elrh_gallery_galleries g ON a.gallery=g.id JOIN elrh_users u ON a.author=u.u_name WHERE a.id='".mysqli_real_escape_string($mysqli, $item)."'");
+				$data["entry"] = ELRHDataExtractor::retrieveRow($mysqli, "SELECT a.id AS aid, a.author, a.cat, a.posted, a.name AS article_name, a.dscr, a.content, g.id AS gid, g.name AS gallery_name, (SELECT count(*) FROM elrh_gallery_images i WHERE i.gallery=g.id) AS images, u.u_displayed_name AS author_name FROM elrh_articles a LEFT JOIN elrh_gallery_galleries g ON a.gallery=g.id JOIN elrh_users u ON a.author=u.u_name WHERE a.id='".mysqli_real_escape_string($mysqli, $item)."'");
 				if (!empty($data["entry"])) {
 					// page title adjustment
 					$data["item_title"] = ": ".$data["entry"]["article_name"];
